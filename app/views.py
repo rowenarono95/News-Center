a6c431acaeb35e1a4a8278807f20350cfd53ae29
rowenarono95 <rowenarono@gmail.com>
2020-10-18 22:54:36 +0300

    add class config

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    message = 'Welcome To News Center'
     return render_template('index.html')
+
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+
+    '''
+    View article page function that returns the article details page and its data
+    '''
+    return render_template('article.html',id = article_id)    
